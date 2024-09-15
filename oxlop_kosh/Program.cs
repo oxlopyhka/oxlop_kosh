0bdbc2c2c82555fe93efad11551b5687f4a96187
User <User@WIN-PUSTAVLL7EP>
2024-09-16 01:05:39 +0500

    Добавьте файлы проекта.

---
+++ oxlop_kosh/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oxlop_kosh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создаем объект Random
+            Random random = new Random();
+
+            // Генерируем случайное число от 1 до 100
+            int randomNumber = random.Next(1, 101); 
+
+            // Выводим сгенерированное число
+            Console.WriteLine("Случайное число: " + randomNumber);
+        }
+    }
+}
